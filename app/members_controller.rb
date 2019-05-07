41117d5828f029b5c80cdbfabe518dfc8a33117f
Preston Elliott <preston.elliott24@gmail.com>
2019-05-07 13:54:40 -0400

    Added models folder and controllers for classes. Updated environment to point to app/models from app

---
+++ app/members_controller.rb
@@ -0,0 +1,7 @@
+#Create methods here
+
+# def can_be_instantiated_and_then_saved
+#     allegiance = Allegiance.new
+#     allegiance.oath = "What is dead may never die."
+#     allegiance.save
+# end
