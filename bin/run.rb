8b14338323ab9df1c2f2794aae99334d6d5cc66e
Will Jones <willjones2010@gmail.com>
2019-05-07 11:41:12 -0400

    Revert "classes user, favorite, and quote set up"
    This reverts commit fd63cff68dfbd03d30fbf2163d4e5ea293073ce6.

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,5 @@
-require_relative '../config/environment.rb'
-require 'pry'
+require_relative '../config/environment'
 
 
-blake = User.new("Blake")
-will = User.new("Will")
-best_quote = will.create_quote("This is the quote of all quotes!", "Roses are red. Violets are blue It dont always be like that but sometimes it do...")
-super_quote = blake.create_quote("This is a super quote!", "Under the sun, when the coding is fun, Ill make up a quote and get things done..")
-will.add_to_favorites(super_quote)
-blake.add_to_favorites(best_quote)
-binding.pry
-0
+
+puts "HELLO WORLD"
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
-require_relative '../bin/user'
-require_relative '../bin/quote'
-require_relative '../bin/favorite'
-
 require 'bundler'
 Bundler.require
 
-# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-# require_all 'lib'
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+require_all 'lib'
