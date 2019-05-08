43f2b6ddefcb718a791cd9cd68fc7fdb18a5a4a1
Preston Elliott <preston.elliott24@gmail.com>
2019-05-08 09:57:51 -0400

    Updated seed file to match schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,35 @@ Allegiance.destroy_all
 targaryen = House.create(
   name: "Targaryen",
   house_crest: "http://assets.viewers-guide.hbo.com/larges1-houses-rgb-sigil-avatar-house-targaryen-1024x1024@2x.jpg",
-  year_founded: 100
+  oath: "Dragon Squad"
 )
 stark = House.create(
   name: "Stark",
   house_crest: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142",
-  year_founded: 123
+  oath: "Winter is Coming"
 )
 greyjoy = House.create(
   name: "Greyjoy",
   house_crest: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20170523015836",
-  year_founded: 54
+  oath: "What is Dead may Never Die"
 )
 baratheon = House.create(
   name: "Baratheon",
   house_crest: "https://awoiaf.westeros.org/images/thumb/2/2d/House_Baratheon.svg/1200px-House_Baratheon.svg.png",
-  year_founded: 160
+  oath: "Ours is the Fury"
 )
 
 
 maryam = Member.create(
   name: "Maryam",
-  hometown: "Winterfell",
   life_status: "Alive"
 )
 preston = Member.create(
   name: "Preston",
-  hometown: "King's Landing",
   life_status: "Alive"
 )
 robert = Member.create(
   name: "Robert Baratheon",
-  hometown: "Storm's End",
   life_status: "Dead"
 )
 
