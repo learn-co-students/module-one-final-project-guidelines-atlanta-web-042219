e4adc87cbdeac2b2296c2d6573726188309001e2
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-05-08 10:16:44 -0400

    Update to seed file to the House classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,42 @@ Member.destroy_all
 Allegiance.destroy_all
 
 targaryen = House.create(
-  name: "Targaryen",
+  name: "House Targaryen",
   house_crest: "http://assets.viewers-guide.hbo.com/larges1-houses-rgb-sigil-avatar-house-targaryen-1024x1024@2x.jpg",
-  year_founded: 100
+  oath: "Fire and Blood"
 )
 stark = House.create(
-  name: "Stark",
+  name: "House Stark",
   house_crest: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142",
-  year_founded: 123
+  oath: "Winter is Coming"
 )
-greyjoy = House.create(
-  name: "Greyjoy",
-  house_crest: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20170523015836",
-  year_founded: 54
+lannister = House.creat(
+  name: "House Lannister",
+  house_crest: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQktGBMO6TTXC6W4LANKuvb94JKrZS3WXx_c4l72FpGYA8MydNCwg",
+  oath: "A Lannister always pays his debts!"
 )
-baratheon = House.create(
-  name: "Baratheon",
-  house_crest: "https://awoiaf.westeros.org/images/thumb/2/2d/House_Baratheon.svg/1200px-House_Baratheon.svg.png",
-  year_founded: 160
-)
+# greyjoy = House.create(
+#   name: "House Greyjoy",
+#   house_crest: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20170523015836",
+#   oath: "What is Dead May Never Die"
+# )
+# baratheon = House.create(
+#   name: "House Baratheon",
+#   house_crest: "https://awoiaf.westeros.org/images/thumb/2/2d/House_Baratheon.svg/1200px-House_Baratheon.svg.png",
+#   oath: "Fire and Blood"
+# )
 
 
 maryam = Member.create(
   name: "Maryam",
-  hometown: "Winterfell",
   life_status: "Alive"
 )
 preston = Member.create(
   name: "Preston",
-  hometown: "King's Landing",
   life_status: "Alive"
 )
 robert = Member.create(
   name: "Robert Baratheon",
-  hometown: "Storm's End",
   life_status: "Dead"
 )
 
