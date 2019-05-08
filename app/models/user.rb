be4d2580d815a39cd527ec3dc1ecb186ef90ab16
Will Jones <willjones2010@gmail.com>
2019-05-08 12:18:29 -0400

    removed title and date from quotes table and added user id to quotes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
   has_many :favorites
   has_many :quotes, through: :favorites
+
+
+  def create_new_quote(title, date, quote_text)
+    Quote.create(title: title, date: date, quote_text: quote_text)
+  end
+
+  def delete_a_favorite_quote(quote_title)
+    Favorite.find_by(name: title).destroy
+  end
+
+
+  # def find_quote_by_title(title)
+  #   if !Quote.find_by(name: title)
+  #     puts "I could not find that quote!"
+  #   else
+  #     Quote.find_by(name: title)
+  #   end
+
+
+  def see_my_favorites
+    Favorite.find_by(user: self)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 6) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -18,9 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "quotes", force: :cascade do |t|
-    t.string  "title"
-    t.integer "date"
     t.string  "quote_text"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
