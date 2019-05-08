9b66460e5e86c6588937e4fc048c73a4149382d3
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-05-08 07:58:06 -0400

    adding CLI to run file

--- bin/run.rb
+++ bin/run.rb
@@ -4,27 +4,31 @@ prompt = TTY::Prompt.new
 puts "Welcome to the Future Game of Thrones!"
 
 sleep(2)
-user_name = prompt.ask('How are you called?', default: ENV['USER'])
-puts "#{user_name} Who's allience will you join?"
+@@user_name = prompt.ask('How are you called?', default: ENV['USER'])
+puts "#{@@user_name} Who's allience will you join?"
 # Once you get the house name, search the House class to get that "id"
-user_house = prompt.select("Choose your house", ['Targarian', 'Lannister', 'Stark'])
+# user_house = prompt.select("Choose your house", ['Targarian', 'Lannister', 'Stark'])
 
-user_name = Member.create(
-  name: "#{user_name.capitalize}",
-  hometown: "Winterfell",
-  life_status: "Alive"
-)
-user_house = House.create(
-name: "#{user_house}",
-house_crest: "url",
-year_founded: 1900
-)
 
-def no
-  puts "GOT RULES"
+def create_new_member
+  Member.create(
+    name: "#{@@user_name.capitalize}",
+    hometown: "Winterfell",
+    life_status: "Alive")
 end
 
-user_name.no
+@@user_name.create_new_member
+# user_house = House.create(
+# name: "#{user_house}",
+# house_crest: "url",
+# year_founded: 1900
+# )
+
+# def no
+#   puts "GOT RULES"
+# end
+#
+# user_name.no
 
 
 # 1. create user_name
