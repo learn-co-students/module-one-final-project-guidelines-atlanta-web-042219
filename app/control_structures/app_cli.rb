5b526e415c5dc5aeab6628682c992e4ad15be915
Andrew Allen <asallen67@gmail.com>
2019-05-09 09:56:07 -0400

    moved ascii global var

--- app/control_structures/app_cli.rb
+++ app/control_structures/app_cli.rb
@@ -7,16 +7,4 @@ class AppCLI
 		# $user = User.find_by(username: "dev")
 		# MainMenu.run
 	end
-
-	$ascii_art =%Q[                                                                                     
-  ,ad8888ba,   88                                                  88               
- d8"'    `"8b  88                                                  88               
-d8'            88                                                  88               
-88             88   ,adPPYba,  ,adPPYYba,  8b,dPPYba,              88  8b       d8  
-88             88  a8P_____88  ""     `Y8  88P'   `"8a             88  `8b     d8'  
-Y8,            88  8PP"""""""  ,adPPPPP88  88       88             88   `8b   d8'   
- Y8a.    .a8P  88  "8b,   ,aa  88,    ,88  88       88     888     88    `8b,d8'    
-  `"Y8888Y"'   88   `"Ybbd8"'  `"8bbdP"Y8  88       88     888     88      Y88'     
-                                                                           d8'      
-                                                                          d8'       ]
 end
--- app/control_structures/global.rb
+++ app/control_structures/global.rb
@@ -7,4 +7,16 @@ class Global
 	$continue_text = $press_enter+" to continue..."
 	$try_again_text = $press_enter+" to try again..."
 	$user = nil
+	
+	$ascii_art =%Q[                                                                                     
+  ,ad8888ba,   88                                                  88               
+ d8"'    `"8b  88                                                  88               
+d8'            88                                                  88               
+88             88   ,adPPYba,  ,adPPYYba,  8b,dPPYba,              88  8b       d8  
+88             88  a8P_____88  ""     `Y8  88P'   `"8a             88  `8b     d8'  
+Y8,            88  8PP"""""""  ,adPPPPP88  88       88             88   `8b   d8'   
+ Y8a.    .a8P  88  "8b,   ,aa  88,    ,88  88       88     888     88    `8b,d8'    
+  `"Y8888Y"'   88   `"Ybbd8"'  `"8bbdP"Y8  88       88     888     88      Y88'     
+                                                                           d8'      
+                                                                          d8'       ]
 end
